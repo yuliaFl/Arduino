3dff377931ad0e22d1c0216dcb5645d4d4dd83e7
YULIA FLENOVA <FLENOVA.YULIA@GMAIL.COM>
2023-10-16 08:56:08 -0400

    new project love-o-meter

---
+++ LOVE_O_METER/LOVE_O_METER.ino
@@ -0,0 +1,44 @@
+const int sensorPin =0;
+const float baselineTemp = 19.0;
+
+void setup() {
+  Serial.begin(9600); // open serial port
+  for (int pinNumber = 2; pinNumber<5; pinNumber++){
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber,LOW);
+  }
+}
+
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+
+  //convert ADC reading to voltage 
+  float voltage = (sensorVal/1024.0) * 5.0;
+
+  Serial.print(" , Volts: ");
+  Serial.print(voltage);
+  Serial.print(" , degrees C: ");
+  //convert the voltage to temperature in degreees
+  float temperature = (voltage - .5)*100;
+  Serial.println(temperature);
+  if(temperature < baselineTemp+2){
+    digitalWrite(2,LOW);
+      digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+  }else if(temperature >+baselineTemp+2 && temperature < baselineTemp+4){
+  digitalWrite(2,HIGH);
+      digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+  }else if(temperature >+baselineTemp+4 && temperature < baselineTemp+6){
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+        digitalWrite(4,LOW);
+  }else if (temperature >= baselineTemp+6){
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+        digitalWrite(4,HIGH);
+  }
+  delay(1);
+}
